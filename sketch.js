7bde6e8ad973811045159c50d6fa72d2bf3ac4ac
sunakshiwhjr <77574525+sunakshiwhjr@users.noreply.github.com>
2021-07-23 14:00:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,23 +2,18 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 
-let engine;
-let world;
+
 
 
 function setup() {
   createCanvas(400,400);
-  engine = Engine.create();
-  
-  world = engine.world;
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
+
   
 }
 
 function draw() 
 {
   background(51);
-  Engine.update(engine);
+
 }
 
